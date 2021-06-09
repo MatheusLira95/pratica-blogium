963400080a64d35496da6f71d7ce09c88ec4a985
Matheus Lira <matheustlira@gmail.com>
2021-06-09 20:10:53 -0300

    Sending posts in NewStory page

--- src/components/NewStoryPage.js
+++ src/components/NewStoryPage.js
@@ -1,16 +1,31 @@
-import { useState } from 'react';
-import axios from 'axios';
-import { useHistory } from 'react-router-dom';
-import PostManipulation from './PostManipulation/PostManipulation';
+import { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import PostManipulation from "./PostManipulation/PostManipulation";
 
 export default function NewStoryPage() {
-  const [title, setTitle] = useState('');
-  const [coverUrl, setCoverUrl] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [coverUrl, setCoverUrl] = useState("");
+  const [content, setContent] = useState("");
   const [isSaveButtonDisabled, setSaveButtonDisable] = useState(false);
   const history = useHistory();
 
-  function onPostSaveButtonClick() {}
+  function onPostSaveButtonClick() {
+    const newContent = content.substring(3, content.length - 4);
+    const body = {
+      title,
+      coverUrl,
+      contentPreview: `${newContent.substring(0, 20)}...`,
+      content: newContent,
+    };
+    const request = axios.post("http://localhost:4000/new-story", body);
+    request.then((res) => {
+      console.log("Deu bom");
+    });
+    request.catch(() => {
+      alert("Deu ruim, tente novamente");
+    });
+  }
 
   return (
     <PostManipulation
